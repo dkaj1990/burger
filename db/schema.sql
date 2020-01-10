cf1012b0ee7a4a81e88182f1f6e9296a2df3b8bd
Dipti Katkar <dk.aj1990@gmail.com>
2020-01-10 15:23:01 -0600

    Added db schema seeds

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+create database burgers_db;
+
+use burgers_db;
+
+create table burgers(
+    id integer not null auto_increment,
+    burger_name varchar(40),
+    devoured boolean,
+    primary key(id) 
+); 
+
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+insert into burgers values ("The original", true); 
+insert into burgers values ("The Company", false); 
+insert into burgers values ("The Veggie", true); 
