42beab24305653db4bc23e6f687dde8eb4886afa
Dipti Katkar <dk.aj1990@gmail.com>
2020-01-26 12:39:22 -0600

    changes to add burger

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require("express"); 
 const exphbs = require("express-handlebars"); 
+const mysql = require("mysql"); 
 
 const PORT = process.env.PORT || 8080; 
 
-app = express(); 
-
-const router = require("./controllers/burgers_controller");
+const app = express(); 
 
 
- app.use(router);
+
+
+
 
 app.use(express.static( 'public'));
 app.use(express.urlencoded({ extended: true }));
@@ -16,8 +17,8 @@ app.use(express.json());
 app.engine("handlebars", exphbs({defaultLayout:"main"}));
 app.set("view engine", "handlebars");
 
-
-
+const routes = require("./controllers/burgers_controller");
+app.use(routes);
 
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
