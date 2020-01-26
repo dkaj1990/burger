c3dcf80c9a1f918c62cce65e6174f13a4c2a73e7
Dipti Katkar <dk.aj1990@gmail.com>
2020-01-26 12:19:08 -0600

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS burgers_db;
 create database burgers_db;
 
 use burgers_db;
@@ -5,7 +6,7 @@ use burgers_db;
 create table burgers(
     id integer not null auto_increment,
     burger_name varchar(40),
-    devoured boolean,
+    devoured boolean default false,
     primary key(id) 
 ); 
 
